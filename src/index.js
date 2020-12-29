5a9e9eb0e606b5f73de707374bae0a761436ada6
Chris Hadley <hadleyc15@yahoo.com>
2020-12-29 15:14:52 -0700

    creating start rating app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { FaStar } from "react-icons/fa";
 
-function App() {
-  const [status, setStatus] = useState("Not Delivered");
-  
-  return (
-    <div>
-      <h1>The package is: {status}.</h1>
-      <button onClick={() => setStatus("Delivered")}>Deliver</button>
-    </div>
-  )
+const createArray = (length) => [
+  ...Array(length)
+]
+
+function Star() {
+  return <FaStar />;
+}
+
+function StarRating() {
+  return createArray(5).map((n, i) => (
+    <Star key={i} />
+  ));
+}
+
+function App() { 
+  return <StarRating />;
 }
 
 ReactDOM.render(
